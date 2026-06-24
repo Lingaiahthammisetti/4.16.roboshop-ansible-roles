70a241a17097897fb649ae43a559806deb7c3e44
Lingaiahthammisetti <sivalingapractise555@gmail.com>
2026-06-24 21:18:16 +0530

    roboshop ansible roles

---
+++ eleven-ec2-instances/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 6.37.0"
+    }
+  }
+
+backend "s3" {
+  bucket = "linga-devops-rs"
+  key = "roboshop-ansible-roles-11-ec2-instances"
+  region = "us-east-1"
+  #dynamodb_table = "roboshop13-locking"
+  use_lockfile = true
+  }
+}
+#provide authentication here
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
